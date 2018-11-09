0b412d30f686ddf145323a1f2abe6564aa719ff2
Chris Nuernberger <chris@thinktopic.com>
2018-11-09 08:52:51 -0700

    1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io "1.0-SNAPSHOT"
+(defproject techascent/tech.io "1.0"
   :description "IO abstractions to enable rapid research and prototyping."
   :url "http://github.com/tech-ascent/tech.io"
   :license {:name "Eclipse Public License"
@@ -6,8 +6,8 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [techascent/tech.resource "2.0"]
                  [me.raynes/fs "1.4.6"]
-                 [com.taoensso/nippy "2.14.0"]
+                 [com.taoensso/nippy "2.15.0-alpha1"]
                  [com.taoensso/timbre "4.10.0"]
                  [techascent/tech.config "0.3.5"]
-                 [org.clojure/core.async "0.3.465"]
+                 [org.clojure/core.async "0.4.474"]
                  [com.stuartsierra/component "0.3.2"]])
