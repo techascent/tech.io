f3d0c4b7a188196b779ce1ce26cb58ddbcac67d6
Chris Nuernberger <chris@thinktopic.com>
2018-11-09 08:53:00 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io "1.0"
+(defproject techascent/tech.io "1.1-SNAPSHOT"
   :description "IO abstractions to enable rapid research and prototyping."
   :url "http://github.com/tech-ascent/tech.io"
   :license {:name "Eclipse Public License"
