b8b673a52388dc84533b2bd5cb3d450102a21b6f
Chris Nuernberger <chris@thinktopic.com>
2019-01-01 15:27:47 -0700

    finally have private deps working.

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
-  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}})
+  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}}
+  :repositories {"releases"  {:url "s3p://techascent.jars/releases/"
+                              :no-auth true
+                              :sign-releases false}})
