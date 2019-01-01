525d7c64932c4b9f73f2b374fe2f823fbaa6f98c
Chris Nuernberger <chris@thinktopic.com>
2019-01-01 13:56:17 -0700

    upgrade to latest nippy.

--- project.clj
+++ project.clj
@@ -1,13 +1,9 @@
-(defproject techascent/tech.io "2.5-SNAPSHOT"
+(defproject techascent/tech.io "2.5"
   :description "IO abstractions to enable rapid research and prototyping."
   :url "http://github.com/tech-ascent/tech.io"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.resource "4.0"]
-                 [clj-commons/fs "1.5.0"]
-                 [com.taoensso/nippy "2.15.0-alpha1"]
-                 [com.taoensso/timbre "4.10.0"]
-                 [techascent/tech.config "0.3.5"]
-                 [techascent/tech.parallel "1.2"]]
-  :profiles {:dev {:dependencies [[techascent/vault-clj "0.2.17"]]}})
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}})
