a72ee9993a4abbca45e67f8dffa85de74551b3a9
Chris Nuernberger <chris@techascent.com>
2019-08-08 13:38:23 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io "3.5"
+(defproject techascent/tech.io "3.6-SNAPSHOT"
   :description "IO abstractions to enable rapid research and prototyping."
   :url "http://github.com/tech-ascent/tech.io"
   :license {:name "Eclipse Public License"
