420bf5b0bb4410b4ad64548e229b15c7a0d0bcd0
Chris Nuernberger <chris@techascent.com>
2020-10-20 08:27:17 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io "3.21"
+(defproject techascent/tech.io "3.22-SNAPSHOT"
   :description "IO abstractions to enable rapid research, prototyping, and cross cloud
 application development."
   :url "http://github.com/tech-ascent/tech.io"
