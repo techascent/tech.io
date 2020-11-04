b65497aa27ab058f0700caf8b230dd051b979aa0
Chris Nuernberger <chris@techascent.com>
2020-11-04 11:45:52 -0700

    4.03

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject techascent/tech.io "4.03-SNAPSHOT"
+(defproject techascent/tech.io "4.03"
   :description "IO abstractions to enable rapid research, prototyping, and cross cloud
 application development."
   :url "http://github.com/tech-ascent/tech.io"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
-                 [techascent/tech.resource "5.01"]
-                 [com.taoensso/nippy "3.0.0"
-                  :exclusions [com.taoensso/encore]]
-                 [com.taoensso/encore "3.7.0"]
+                 [techascent/tech.resource "5.02"]
+                 ;;Support for
+                 [com.taoensso/nippy "3.1.0-RC1"]
                  [clj-commons/fs "1.5.2"]
                  [techascent/tech.config "0.3.7"]
                  [org.clojure/data.json "1.0.0"]
