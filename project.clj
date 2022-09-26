f92b0ac862c777e3fabec3edd325e48b0ecc40a4
Chris Nuernberger <chris@techascent.com>
2022-09-26 07:32:47 -0600

    4.20

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io "4.20-SNAPSHOT"
+(defproject techascent/tech.io "4.20"
   :description "IO abstractions to enable rapid research, prototyping, and cross cloud
 application development."
   :url "http://github.com/tech-ascent/tech.io"
@@ -9,7 +9,7 @@ application development."
                  [com.taoensso/nippy "3.2.0"]
                  [babashka/fs "0.0.3"]
                  [techascent/tech.config "0.3.13"]
-                 [com.cnuernber/charred "1.012"]]
+                 [com.cnuernber/charred "1.013"]]
   :profiles {:dev {:dependencies [
                                   ;;[techascent/vault-clj "0.2.21"]
                                   [amperity/vault-clj "0.7.0"]
